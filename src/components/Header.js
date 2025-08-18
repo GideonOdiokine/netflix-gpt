d1aa2dee355af1979525b1c9d5fe73f8282047fa
gideonodiokine <gidysmartodiokineh@gmail.com>
2025-08-18 17:35:09 +0100

    feat: MovieList and MovieCard

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,12 +18,13 @@ const Header = () => {
   };
 
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between items-center">
-      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
+    <div className="mx-auto w-[100%] ">
+      <div className="absolute w-full   px-[50px] py-2 bg-gradient-to-b from-black z-10 flex flex-col md:flex-row justify-between items-center">
+        <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
 
-      {user && (
-        <div className="flex p-2 justify-between items-center">
-          {/* {showGptSearch && (
+        {user && (
+          <div className="flex p-2 justify-between items-center">
+            {/* {showGptSearch && (
             <select
               className="p-2 m-2 bg-gray-900 text-white"
               onChange={handleLanguageChange}
@@ -35,22 +36,23 @@ const Header = () => {
               ))}
             </select>
           )} */}
-          {/* <button
+            {/* <button
             className="py-2 px-4 mx-4 my-2 bg-purple-800 text-white rounded-lg"
             // onClick={handleGptSearchClick}
           >
             {showGptSearch ? "Homepage" : "GPT Search"}
           </button> */}
-          <img
-            className="hidden md:block w-12 h-12"
-            alt="usericon"
-            src={user?.photoURL}
-          />
-          <button onClick={handleSignOut} className="font-bold text-white ">
-            (Sign Out)
-          </button>
-        </div>
-      )}
+            <img
+              className="hidden md:block w-12 h-12"
+              alt="usericon"
+              src={user?.photoURL}
+            />
+            <button onClick={handleSignOut} className="font-bold text-white ">
+              (Sign Out)
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
--- src/reduxSlice/moviesSlice.js
+++ src/reduxSlice/moviesSlice.js
@@ -4,15 +4,24 @@ const movieSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
+    popularMovies: null,
+    trailerVideo: null,
   },
 
   reducers: {
     addNowPlayingMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
+    addPopularMovies: (state, action) => {
+      state.popularMovies = action.payload;
+    },
+    addTrailerVideo: (state, action) => {
+      state.trailerVideo = action.payload;
+    },
   },
 });
 
-export const { addNowPlayingMovies } = movieSlice.actions;
+export const { addNowPlayingMovies, addPopularMovies, addTrailerVideo } =
+  movieSlice.actions;
 
 export default movieSlice.reducer;
